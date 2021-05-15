d03799d658fbadf60200036bc3322c6bdf283008
David Betteridge <davidabetteridge@gmail.com>
2021-05-15 22:17:24 +0100

    Basic application

---
+++ README.md
@@ -0,0 +1,3 @@
+yarn set version berry
+yarn create react-app example-app --template typescript
+yarn add eslint-config-react-app
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Example Application
+      </header>
+    </div>
+  );
+}
+
+export default App;
